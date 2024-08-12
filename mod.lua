31eea2663f3552b85b229eed9170610683444d84
Dylan7654 <121550283+Dylan7654@users.noreply.github.com>
2024-08-12 18:04:37 +0200

    Add files via upload

---
+++ mod.lua
@@ -0,0 +1,74 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "train", "siemens", "electric", "multi-system", "universal", "es64f4" },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+        params = {
+			{
+				key = "br117_vorspann",
+				name = _("Vorspann_br117"),
+				values = { "No", "Yes", },
+				tooltip = _("option_vorspann_br117_desc"),
+				defaultIndex = 0,
+			},
+			{
+				key = "br117_fake",
+				name = _("Fake_br117"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_br117_desc"),
+				defaultIndex = 0,
+			},
+        },
+	},
+	options = {
+	},
+	runFn = function (settings, modParams)
+
+		local vorspannFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.br117 and data.metadata.br117.vorspann == true then
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2
+				--return false
+			end
+			--return true
+			return data
+		end
+
+		local fakeFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.br117 and data.metadata.br117.fake == true then
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2
+			end
+			return data
+		end
+
+		if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["br117_vorspann"] == 0 then
+				--addFileFilter("model/transportVehicle", vorspannFilter)
+				addModifier("loadModel", vorspannFilter)
+			end
+			if params["br117_fake"] == 0 then
+				addModifier("loadModel", fakeFilter)
+			end
+
+		else
+			--addFileFilter("model/transportVehicle", vorspannFilter)
+			addModifier("loadModel", fakeFilter)
+		end
+	end
+}
+end
---
+++ strings.lua
@@ -0,0 +1,58 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "DB Class 117",
+		["mod_desc"] = "The ES64F4 electric locomotive is a locomotive platform from Siemens Mobility and a member of the EuroSprinter family. In Germany, they are primarily used by Deutsche Bahn as the 189 series. They can be used in all four traction current systems commonly used in Europe. ",
+
+        ["br117_menu"] = 		"Class 117 (ES64F4)",
+        ["br117_fake_menu"] = 	"Class 117 (ES64F4) Fakes",
+		["br117_desc"] = "The ES64F4 vehicles are mainline locomotives for interoperable freight transport. Different variants were realised in order to meet the different requirements of the operators.",
+
+		["br117_siemens_name"] = 	"Class 189 917 Siemens - Grenzenlose Mobilität (2007)",
+
+
+        -- Vorspann
+        ["Vorspann_br117"] = "Active lead engines?",
+        ["option_vorspann_br117_desc"] = "Active the lead engines (with front panto raised)?",
+
+		["br117_db_name_vorspann"] = 		"Lead - Class 117 076 DB Cargo",
+		
+		
+        -- Fake
+        ["Fake_br117"] = "Active fake engines?",
+        ["option_fake_br117_desc"] = "Active the fake engines (pantos down, no light, no driver)?",
+
+		["e17_0_name_fake"] = 	"Fake - E17 106 (1928)",
+		["br117_0_name_fake"] = "Fake - Class 117 106 (1968)",
+
+	},
+	de = {
+
+		["mod_name"] = "DB BR 117",
+		["mod_desc"] = "Die Elektrolokomotive ",
+
+        ["br117_menu"] = 			"DB BR 117",
+        ["br117_menu_fake"] = 		"DB BR 117 Fakes",
+        ["br117_menu_vorspann"] = 	"DB BR 117 Vorspann",
+		["br117_desc"] = "	füllen			",
+
+		["e17_drg_name"] = 		"E17 106 (1928)",
+		["e17_drg2_name"] = 	"E17 106 (1928)",
+		["e17_drg3_name"] = 	"E17 106 (1928)",
+
+        -- Vorspann
+        ["Vorspann_br117"] = "Vorspann-Loks aktivieren?",
+        ["option_vorspann_br117_desc"] = "Die Vorspann-Loks (mit vorderem Panto gehoben) aktiviert werden?",
+		
+		["e17_0_name_vorspann"] = 	"Vorspann - E17 106 (1928)",
+
+        -- Fake
+        ["Fake_br117"] = "Fake-Loks aktivieren?",
+        ["option_fake_br117_desc"] = "Die Fake-Loks (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+		
+		["e17_0_name_fake"] = 	"Fake - E17 106 (1928)",
+
+	},
+}
+end
