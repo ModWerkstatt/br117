4aea513ca3705e3dc08cb359f4a77a4a392c7a40
Florian Thiel <flth@flth.de>
2024-09-15 18:30:32 +0200

    Strings angepasst

--- strings.lua
+++ strings.lua
@@ -2,11 +2,11 @@ function data()
 return {
 	en = {
 
-		["mod_name"] = "DB E 17 / Class 117",
+		["mod_name"] = "DRG/DB/DR E 17 / Class 117",
 		["mod_desc"] = "The class E 17 electric locomotives were procured at the end of the 1920s for express train services. With a drive developed by AEG, they brought a competing drive type to the Buchli drive of the E 16. The locomotives were intended for use primarily in mountainous regions, e.g. in the southern German network and in Silesia. The DR took over 2 locomotives, while 26 remained with the DB. These were to be modernised from 1960, but the complex conversion with parts from the standard locomotives was cancelled after 4 locomotives due to the high costs. The last DB locomotive was taken out of service in 1980, while the DR only used them until 1968.",
 
-        ["br117_menu"] = 			"DB Class 117",
-        ["br117_menu_fake"] = 		"DB Class 117 Fakes",
+        ["br117_menu"] = 			"E 17 / Class 117",
+        ["br117_menu_fake"] = 		"E 17 / Class 117 Fakes",
 		["br117_desc"] = "The class E 17 electric locomotives were procured at the end of the 1920s for express train services. With a drive developed by AEG, they brought a competing drive type to the Buchli drive of the E 16. The locomotives were intended for use primarily in mountainous regions, e.g. in the southern German network and in Silesia. The DR took over 2 locomotives, while 26 remained with the DB. These were to be modernised from 1960, but the complex conversion with parts from the standard locomotives was cancelled after 4 locomotives due to the high costs. The last DB locomotive was taken out of service in 1980, while the DR only used them until 1968.",
 
 		["e17_dr_name"] = 		"DR E 17 bottle green (1946)",
@@ -17,9 +17,12 @@ return {
 		["e17_db5_name"] = 		"DB Class 117 chrome oxide green, red chassis (1968)",
 		["e17_db6_name"] = 		"DB Class 117 chrome oxide green (1974)",
 		["e17_db7_name"] = 		"DB Class 117 chrome oxide green (1974)",
-		["e17_db8_name"] = 		"DB Class 117 chrome oxide green (1960)",
+		["e17_db8_name"] = 		"DB E 17 chrome oxide green (1960)",
+        ["e17_db9_name"] =      "DB BR 117 chrome oxide green (1968)",
 		["e17_drg_name"] = 		"DRG E 17 Middle Germany (1928)",
 		["e17_drg2_name"] = 	"DRG E 17 Silesia (1928)",
+		["e17_drg3_name"] = 		"DRG E 17 Middle Germany [120 km/h] (1934)",
+		["e17_drg4_name"] = 	"DRG E 17 Silesia [120 km/h] (1934)",
 
         -- Fake
         ["Fake_br117"] = "Fake-Loks aktivieren?",
@@ -33,18 +36,21 @@ return {
 		["e17_db5_name_fake"] = 	"Fake - DB Class 117 chrome oxide green, red chassis (1968)",
 		["e17_db6_name_fake"] = 	"Fake - DB Class 117 chrome oxide green (1974)",
 		["e17_db7_name_fake"] = 	"Fake - DB Class 117 chrome oxide green (1974)",
-		["e17_db8_name_fake"] = 	"Fake - DB Class 117 chrome oxide green (1960)",
+		["e17_db8_name_fake"] = 	"Fake - DB E 17 chrome oxide green (1960)",
+        ["e17_db9_name_fake"] =     "Fake - DB BR 117 chromoxidgrün (1968)",
 		["e17_drg_name_fake"] =		"Fake - DRG E 17 Middle Germany (1928)",
 		["e17_drg2_name_fake"] = 	"Fake - DRG E 17 Silesia (1928)",
+		["e17_drg3_name_fake"] = 	"Fake - DRG E 17 Middle Germany [120 km/h] (1934)",
+		["e17_drg4_name_fake"] = 	"Fake - DRG E 17 Silesia [120 km/h] (1934)",
 
 	},
 	de = {
 
-		["mod_name"] = "DB E 17 / BR 117",
+		["mod_name"] = "DRG/DB/DR E 17 / BR 117",
 		["mod_desc"] = "Die Elektrolokomotive der Baureihe E 17 wurden Ende 1920er Jahre für den Schnellzugverkehr beschafft. Mit einem von AEG entwickelten Antrieb brachten sie eine konkurrierende Antriebsart zum Buchli-Antrieb der E 16. Die Loks waren für den Einsatz vor allem in bergigen Regionen gedacht, so z.B. im süddeutschen Netz und in Schlesien. Die DR übernahm 2 Maschinen, bei der DB verblieben 26. Diese sollten ab 1960 modernisiert werden, der aufwendige Umbau mit Teilen der Einheitsloks wurde aber aufgrund der hohen Kosten nach 4 Maschinen abgebrochen. Bei der DB wurde die letzte Maschine 1980 ausgemustert, bei der DR waren sie nur bis 1968 im Einsatz.",
 
-        ["br117_menu"] = 			"DB BR 117",
-        ["br117_menu_fake"] = 		"DB BR 117 Fakes",
+        ["br117_menu"] = 			"E 17 / BR 117",
+        ["br117_menu_fake"] = 		"E 17 / BR 117 Fakes",
 		["br117_desc"] = "Die Elektrolokomotive der Baureihe E 17 wurden Ende 1920er Jahre für den Schnellzugverkehr beschafft. Mit einem von AEG entwickelten Antrieb brachten sie eine konkurrierende Antriebsart zum Buchli-Antrieb der E 16. Die Loks waren für den Einsatz vor allem in bergigen Regionen gedacht, so z.B. im süddeutschen Netz und in Schlesien. Die DR übernahm 2 Maschinen, bei der DB verblieben 26. Diese sollten ab 1960 modernisiert werden, der aufwendige Umbau mit Teilen der Einheitsloks wurde aber aufgrund der hohen Kosten nach 4 Maschinen abgebrochen. Bei der DB wurde die letzte Maschine 1980 ausgemustert, bei der DR waren sie nur bis 1968 im Einsatz.",
 
 		["e17_dr_name"] = 		"DR E 17 flaschengrün (1946)",
@@ -55,9 +61,12 @@ return {
 		["e17_db5_name"] = 		"DB BR 117 chromoxidgrün, rotes Fahrwerk (1968)",
 		["e17_db6_name"] = 		"DB BR 117 chromoxidgrün (1974)",
 		["e17_db7_name"] = 		"DB BR 117 chromoxidgrün (1974)",
-		["e17_db8_name"] = 		"DB BR 117 chromoxidgrün (1960)",
+		["e17_db8_name"] = 		"DB E 17 chromoxidgrün (1960)",
+        ["e17_db9_name"] =      "DB BR 117 chromoxidgrün (1968)",
 		["e17_drg_name"] = 		"DRG E 17 Mitteldeutschland (1928)",
 		["e17_drg2_name"] = 	"DRG E 17 Schlesien (1928)",
+		["e17_drg3_name"] = 		"DRG E 17 Mitteldeutschland [120 km/h] (1934)",
+		["e17_drg4_name"] = 	"DRG E 17 Schlesien [120 km/h] (1934)",
 
         -- Fake
         ["Fake_br117"] = "Fake-Loks aktivieren?",
@@ -71,9 +80,12 @@ return {
 		["e17_db5_name_fake"] = 	"Fake - DB BR 117 chromoxidgrün, rotes Fahrwerk (1968)",
 		["e17_db6_name_fake"] = 	"Fake - DB BR 117 chromoxidgrün (1974)",
 		["e17_db7_name_fake"] = 	"Fake - DB BR 117 chromoxidgrün (1974)",
-		["e17_db8_name_fake"] = 	"Fake - DB BR 117 chromoxidgrün (1960)",
+		["e17_db8_name_fake"] = 	"Fake - DB E 17 chromoxidgrün (1960)",
+        ["e17_db9_name_fake"] =     "Fake - DB BR 117 chromoxidgrün (1968)",
 		["e17_drg_name_fake"] =		"Fake - DRG E 17 Mitteldeutschland (1928)",
 		["e17_drg2_name_fake"] = 	"Fake - DRG E 17 Schlesien (1928)",
+		["e17_drg3_name_fake"] = 	"Fake - DRG E 17 Mitteldeutschland [120 km/h] (1934)",
+		["e17_drg4_name_fake"] = 	"Fake - DRG E 17 Schlesien [120 km/h] (1934)",
 
 	},
 }
